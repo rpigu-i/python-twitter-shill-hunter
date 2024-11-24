604d189622bf9ca3459daa6603d88a545254757c
Andy D <8628078+rpigu-i@users.noreply.github.com>
2024-11-24 08:26:42 -0500

    Update setup.py
    Start updating to Python 3 and for use against X API

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='twitter-shill-hunter',
-    version='0.0.1',
-    description='Uses NLP techniques to hunt for potential bot/shill/sock puppet accounts on Twitter',
-    maintainer='@patamechanix',
+    version='2.0',
+    description='Uses NLP techniques to hunt for potential bot/shill/sock puppet accounts on X',
+    maintainer='@rpigu-i',
     license='MIT',
-    url='https://github.com/patamechanix/pata-password-cracker',
+    url='https://github.com/rpigu-i/pata-password-cracker',
     package_dir={'': 'src'},
     include_package_data=True,
     packages=find_packages('src'),
